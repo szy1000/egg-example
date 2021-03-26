b462b44fbaa84024adfbe416941431aef1dc993c
szy1000 <1538078053@qq.com>
2021-03-26 23:42:48 +0800

    create table

--- app/model/user.js
+++ app/model/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = app => {
-  const { STRING, INTEGER } = app.Sequelize;
+  const { STRING, INTEGER, TEXT, DATE } = app.Sequelize;
 
   return app.model.define('user', {
     id: {
@@ -9,8 +9,13 @@ module.exports = app => {
       primaryKey: true,
       autoIncrement: true,
     },
-    name: STRING(20),
-    pwd: STRING(20),
+    username: STRING(20),
+    password: STRING(64),
+    avatar: TEXT('long'),
+    phone: STRING(20),
+    sign: STRING(300),
+    createTime: DATE,
+    updateTime: DATE,
   });
 };
 
