8bb1cac883de75294559dde5e409febf7470da27
szy1000 <1538078053@qq.com>
2021-04-16 00:26:30 +0800

    fixed

--- app/lib/plugin/egg-auth/app/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = options => {
-  return async (ctx, next) => {
-    const url = ctx.request.url;
-    console.log('url', url);
-    const user = ctx.session.user;
-    console.log(user);
-  };
-};
